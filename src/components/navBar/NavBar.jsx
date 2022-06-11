2f3b5fcaaee2d2d62bb75d87689293e4956446b2
Leonidas <jesus.leon1@unmsm.edu.pe>
2022-06-10 22:48:03 -0500

    feat: add styles navbar component

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -5,16 +5,20 @@ import "./navBar.css";
 const NavBar = () => {
   return (
     <div className="navBar">
-        <span className="logo">Lion app</span>
-        <ul className="list">
-            <li className="listItem">
-                <img src="" alt="" className="avatar" />
-            </li>
-            <li className="listItem">Leonidas Lion</li>
-            <li className="listT">Logout</li>
-        </ul>
+      <span className="logo">Lion app</span>
+      <ul className="list">
+        <li className="listItem">
+          <img
+            src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="avatar"
+          />
+        </li>
+        <li className="listItem">Leonidas Lion</li>
+        <li className="listT">Logout</li>
+      </ul>
     </div>
-  )
+  );
 }
 
 export default NavBar
