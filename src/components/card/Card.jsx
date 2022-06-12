3a4549764b1a8727feb1bbe69bf4f4b81b78b1b8
Leonidas <jesus.leon1@unmsm.edu.pe>
2022-06-11 22:06:44 -0500

    style: upgrade card component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
+import "./card.css"
+
 const Card = ({post}) => {
     return (
         <div className='card'>
-            {post.desc}
+            <span className="title">
+                {post.title}
+                <img src={post.img} alt="" className="img" />
+                <p className="desc">{post.desc}</p>
+                <button className="cardButton">Read More</button>
+            </span>
         </div>
     )
 }
